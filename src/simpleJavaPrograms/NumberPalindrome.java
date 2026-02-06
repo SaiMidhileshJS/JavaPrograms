c154a97f46419b9e3ebb5a22a5647fd434b01b29
midhilesh <midhileshchowdhary@gmail.com>
2026-02-06 23:37:00 +0530

    Added a print statement in NumberPalindrome.java

--- src/simpleJavaPrograms/NumberPalindrome.java
+++ src/simpleJavaPrograms/NumberPalindrome.java
@@ -3,7 +3,8 @@ package simpleJavaPrograms;
 public class NumberPalindrome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Work in Progress for this NumberPalindrome.java");
 
 	}
 
